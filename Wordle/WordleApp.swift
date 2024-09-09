a85035bf251a1e9ebe77e506af6d32f7cb16627c
jqson <yuanfengjiao@gmail.com>
2024-09-08 19:36:32 -0700

    Init project

---
+++ Wordle/WordleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordleApp.swift
+//  Wordle
+//
+//  Created by Yuanfeng Jiao on 9/8/24.
+//
+
+import SwiftUI
+
+@main
+struct WordleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
