6d82dbefa57fec7d4d967c25a80d5436f66c5719
jqson <yuanfengjiao@gmail.com>
2024-09-16 15:16:07 -0700

    Generate target word

--- Wordle/Views/ContentView.swift
+++ Wordle/Views/ContentView.swift
@@ -8,29 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    enum Constants {
+        static var wordLenght: Int = 5
+    }
+    
     @Environment(ModelData.self) var modelData
     
     @State private var inputString: String = ""
+    @State private var targetWord: String = ""
     
     var body: some View {
-
-        VStack(alignment: .center) {
-            WordListView().environment(modelData)
-            TextField(
-                "Input",
-                text: $inputString
-            )
-            .multilineTextAlignment(.center)
-        }
-        .padding()
-        
-        Button(
-            "Confirm",
-            action: {
-                modelData.guesses.append(.init(id: modelData.guesses.count, text: inputString))
-                inputString = ""
+        NavigationView {
+            VStack(alignment: .center) {
+                WordListView().environment(modelData)
+                TextField(
+                    "Input",
+                    text: $inputString
+                )
+                .multilineTextAlignment(.center)
+                
+                Button(
+                    "Confirm",
+                    action: {
+                        modelData.guesses.append(.init(id: modelData.guesses.count, text: targetWord))
+                        inputString = ""
+                    }
+                )
             }
-        )
+            .padding()
+        }.onAppear(perform: generateWord)
+    }
+    
+    private func generateWord() {
+        if let word = WordManager(wordLength: Constants.wordLenght).getRandomWord() {
+            targetWord = word
+        } else {
+            assertionFailure("Failed to find target word.")
+        }
     }
 }
 
--- Wordle/WordManager.swift
+++ Wordle/WordManager.swift
@@ -11,12 +11,11 @@ import UIKit
 class WordManager {
     
     enum Constants {
-        static var fileName: String = "Resources/english-words"
+        static var fileName: String = "english-words"
         static var fileType: String = "txt"
-        static var maxTry: Int = 3
     }
     
-    var wordList: [String] = []
+    private var wordList: [String] = []
 
     init(wordLength: Int) {
         if let fileURL = Bundle.main.url(forResource: Constants.fileName, withExtension: Constants.fileType) {
@@ -24,7 +23,6 @@ class WordManager {
                 let fileString = try String(contentsOf: fileURL)
                 let allWords: [String] = fileString.components(separatedBy: "\n")
                 wordList = allWords.filter({ $0.count == wordLength && checkWord(word: $0) }).map { $0.uppercased() }
-                print(wordList)
             } catch {
                 print("Load file error.")
             }
